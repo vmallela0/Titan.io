ac73521823bfd42925704ff850c2cfd50ece3651
anyas15 <54959959+anyas15@users.noreply.github.com>
2019-09-30 17:50:06 -0400

    Update menu.lua
    fix buttons

--- Titan.io/menu.lua
+++ Titan.io/menu.lua
@@ -35,11 +35,11 @@ function scene:create( event )
 	local playButton = display.newText(sceneGroup, "Play", display.contentCenterX, 455, native.systemFont, 100)
 	playButton:setFillColor(0, 0, 0)
 
-	local playButton = display.newText(sceneGroup, "High Scores", display.contentCenterX, 650, native.systemFont, 80)
-	playButton:setFillColor(0, 0, 0)
+	local highscoresButton = display.newText(sceneGroup, "High Scores", display.contentCenterX, 650, native.systemFont, 80)
+	highscoresButton:setFillColor(0, 0, 0)
 
 	playButton:addEventListener("tap", gotoGame)
-	playButton:addEventListener("tap", gotoHighScores)
+	highscoresButton:addEventListener("tap", gotoHighScores)
 end
 
 -- show()
