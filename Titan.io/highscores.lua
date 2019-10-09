0318c4c0c0bc10b70e533f57f95eef1aa01825ad
Vedu Mallela <42554579+quantum509@users.noreply.github.com>
2019-10-09 16:44:45 -0400

    Update highscores.lua
    Changes scene for background highscore.

--- Titan.io/highscores.lua
+++ Titan.io/highscores.lua
@@ -22,7 +22,7 @@ function scene:create( event )
 
 	local sceneGroup = self.view
 	-- Code here runs when the scene is first created but has not yet appeared on screen
-	local background = display.newImageRect(sceneGroup, "gamebackground.png", 1400, 800)
+	local background = display.newImageRect(sceneGroup, "background.png", 1400, 800)
 	background.x = display.contentCenterX
 	background.y = display.contentCenterY
 end
